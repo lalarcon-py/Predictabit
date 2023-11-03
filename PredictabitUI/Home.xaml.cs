944cfbe81e980259c4a2dc257a5447a7e487cd5e
Luis Alarcon <luis@pulseb2b.com>
2023-11-02 18:16:07 -0700

    rename namespace in csproj file, change MainWindow to Home for easier navigation, WIP implementation for nav bar

---
+++ PredictabitUI/Home.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using Predictabit;
+
+namespace PredictabitUI
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home
+    {
+        private Keylogger _keyLogger = new Keylogger();
+        public Home()
+        {
+            InitializeComponent();
+            _keyLogger.StartKeyLogging();
+        }
+        
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle "New" action
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle "Open" action
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle "Exit" action
+            Close();
+        }
+
+        private void TabStatistics_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle "Tab Statistics" action
+        }
+    }
+}
